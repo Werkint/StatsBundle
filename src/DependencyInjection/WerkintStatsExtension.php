4e3bd0482cff3e9d38d3e89fce728b3936c5821f
Bogdan Yurov <bogdan@yurov.me>
2014-05-26 01:45:04 +0300

    Config

--- src/DependencyInjection/WerkintStatsExtension.php
+++ src/DependencyInjection/WerkintStatsExtension.php
@@ -3,6 +3,7 @@ namespace Werkint\Bundle\StatsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -23,7 +24,7 @@ class WerkintStatsExtension extends Extension
     ) {
         $processor = new Processor();
         $config = $processor->processConfiguration(
-            new Configuration($this->getAlias()),
+            $this->getConfiguration($configs, $container),
             $configs
         );
         $container->setParameter(
@@ -37,4 +38,14 @@ class WerkintStatsExtension extends Extension
         $loader->load('services.yml');
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfiguration(array $config, ContainerBuilder $container)
+    {
+        $config = new Configuration($this->getAlias());
+        $r = new \ReflectionClass($config);
+        $container->addResource(new FileResource($r->getFileName()));
+        return $config;
+    }
 }
